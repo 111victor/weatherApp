049d42ed697bd6242093f92408e7f8a325ec4576
qingyang <1234567victor@163.com>
2018-10-23 16:47:10 -0700

    using axios lib

---
+++ playground/promise-2.js
@@ -0,0 +1,28 @@
+const request = require('request');
+var geocodeAddress = (address) =>{
+	return new Promise((resolve,reject)=>{
+		var urlIncode = encodeURIComponent(address);
+		request({
+			url:`https://maps.googleapis.com/maps/api/geocode/json?address=${urlIncode}&key=AIzaSyBqKyBy0JtWyFE3RLUHAzeRZY8W1EugUTk`,
+			json: true
+		},(error, response, body)=>{
+			if(error){
+				reject('Unable to connect to Google servers.');
+			}else if(body.status === 'ZERO_RESULTS'){
+				reject('Unable to find that address');
+			}else if(body.status === 'OK'){
+				resolve({
+					address: body.results[0].formatted_address,
+					latitude: body.results[0].geometry.location.lat,
+					longitude: body.results[0].geometry.location.lng
+				});
+			}
+		})
+	});
+};
+
+geocodeAddress('19146').then((location)=>{
+	console.log(JSON.stringify(location,undefined,2));
+},(errorMessage)=>{
+	console.log(errorMessage);
+})
